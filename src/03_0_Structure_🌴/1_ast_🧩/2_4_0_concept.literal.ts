39aa8412c4689a250a22d5018a36140f589a0098
Jeremy Evans <openspacehexagon@gmail.com>
2022-06-11 19:04:26 -0400

    WIP concept projection

--- src/03_0_Structure_🌴/1_ast_🧩/2_4_0_concept.literal.ts
+++ src/03_0_Structure_🌴/1_ast_🧩/2_4_0_concept.literal.ts
@@ -1,13 +1,14 @@
 import { NodeName } from "wrapt.co_re/dist/Domain [╍🌐╍🧭╍]/syntax/0_1_2_2_structure-implementation.enum.js";
 import { Node }     from "wrapt.co_re/dist/Domain [╍🌐╍🧭╍]/syntax/0_1_0_structure-concept.js";
 import { UnParser } from "wrapt.co_re/dist/Domain [╍🌐╍🧭╍]/system/un-parser";
+import { ConceptProjection } from "./2_4_2_concept-projection";
 
 export class ConceptLiteral implements Node {
     NodeName = NodeName.ConceptLiteral;
 
-    constructor() {
-
-    }
+    constructor(
+        public projections: ConceptProjection[]
+    ) { }
 
     public UnParse(ctx: UnParser) {
         return ctx.transpile(this);
